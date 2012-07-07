9c42b848202c92667e36261c761de3a49eca1cea
Benjamin Peterson <benjamin@python.org>
2012-07-07 11:06:36 -0700

    basic support for filing and viewing bugs

--- mozugs/models.py
+++ mozugs/models.py
@@ -1,3 +1,12 @@
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 ModelBase = declarative_base()
+
+
+class Bug(ModelBase):
+    __tablename__ = "bugs"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description = Column(String)
--- mozugs/urls.py
+++ mozugs/urls.py
@@ -5,4 +5,6 @@ from werkzeug.routing import Map, Rule
 
 url_map = Map([
     Rule("/", endpoint="index"),
+    Rule("/newbug/", endpoint="new_bug"),
+    Rule("/bug/<int:bugid>/", endpoint="view_bug"),
 ], redirect_defaults=False)
