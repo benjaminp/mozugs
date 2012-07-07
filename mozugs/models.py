1baf1bb45e25ce568c9ce0c7f7ea9dcc77563d12
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2012-07-07 11:21:18 -0700

    add basic User model

--- mozugs/models.py
+++ mozugs/models.py
@@ -1,3 +1,17 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 ModelBase = declarative_base()
+
+
+class User(ModelBase):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<User(%s)>" % self.name
